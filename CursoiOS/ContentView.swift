0e7cf1717f25eed58601db8e23443e351152627a
Fernando <ferdiego88@gmail.com>
2024-08-08 11:03:21 -0700

    Initial Commit

---
+++ CursoiOS/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  CursoiOS
+//
+//  Created by Diego Santillan on 8/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .frame(height: 40)
+            Rectangle()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .frame(height: 40)
+                .padding(.bottom,60)
+            Rectangle()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .frame(height: 100)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Diego Santillan on 8/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
