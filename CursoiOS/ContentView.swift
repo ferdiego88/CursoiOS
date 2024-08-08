a472ce5a34b0a1b285000db5c5cd7f46622ed540
Fernando <ferdiego88@gmail.com>
2024-08-08 11:58:56 -0700

    Crear ejercicio rectangulos

--- CursoiOS/ContentView.swift
+++ CursoiOS/ContentView.swift
@@ -10,18 +10,47 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Rectangle()
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .frame(height: 40)
-            Rectangle()
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .frame(height: 40)
-                .padding(.bottom,60)
-            Rectangle()
-                .foregroundColor(.red)
-                .frame(height: 100)
+            VStack {
+                HStack {
+                    Rectangle()
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .padding(.bottom, 8)
+                    Rectangle()
+                        .foregroundColor(.orange)
+                        .padding(.bottom,8)
+                    Rectangle()
+                        .foregroundColor(.yellow)
+                        .padding(.bottom,8)
+                }.background(.red)
+                    .padding(.bottom, 100)
+                HStack {
+                    Circle()
+                        .foregroundColor(.green)
+                    Rectangle()
+                        .padding(.vertical,10)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                    Circle()
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }.background(.red)
+
+                HStack {
+                    Rectangle()
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .padding(.top,8)
+                    Rectangle()
+                        .foregroundColor(.orange)
+                        .padding(.top,8)
+                    Rectangle()
+                        .foregroundColor(.yellow)
+                        .padding(.top,8)
+                }.background(.red)
+                    .padding(.top, 100)
+            }
+            .background(.orange)
+            .frame(height: 700)
         }
-        .padding()
+        
+        
     }
 }
 
