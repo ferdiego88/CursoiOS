e7912c97c9964621b9e16fcc8ef54dfffc9dc1d1
Fernando <ferdiego88@gmail.com>
2024-08-15 21:13:15 -0700

    Agregar componentes TextField y SecureField

--- CursoiOS/Components/TextFieldExample.swift
+++ CursoiOS/Components/TextFieldExample.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct TextFieldExample: View {
     @State var email = ""
+    @State var password = ""
     var body: some View {
-        TextField("Escribe tu email", text: $email)
-            .keyboardType(.emailAddress)
+        VStack {
+            TextField("Escribe tu email", text: $email)
+                .keyboardType(.emailAddress)
+                .padding(16)
+                .background(.gray.opacity(0.2))
+                .cornerRadius(16)
+                .padding(.horizontal,16)
+                .onChange(of: email) { oldValue, newValue in
+                    print("El antiguo valor era \(oldValue) y el nuevo es\(newValue)")
+            }            
+            SecureField("Escribe tu password", text: $password)
+                .keyboardType(.emailAddress)
+                .padding(16)
+                .background(.gray.opacity(0.2))
+                .cornerRadius(16)
+                .padding(.horizontal,16)
+                .onChange(of: password) { oldValue, newValue in
+                    print("El antiguo valor era \(oldValue) y el nuevo es\(newValue)")
+            }
+        }
+
     }
 }
 
